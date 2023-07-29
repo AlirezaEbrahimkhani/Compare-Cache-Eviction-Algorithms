3182ff5cba76b2c753457d837ce6b5229e93123f
AlirezaEbrahimkhani <alirezaebrahimkhanidev@gmail.com>
2023-07-29 23:37:58 +0330

    refactor: use utility classes and make request to server

--- client/main.py
+++ client/main.py
@@ -1,14 +1,14 @@
-import json
-  
-# Opening JSON file
-f = open('tweets.json')
-  
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
-  
-for i in range(0, 100):
-    print(data[i])
-  
-# Closing file 
-f.close()
+import requests
+from time import sleep
+
+class Main:
+    
+    def getTweet(self, id: int):
+        r = requests.get('http://localhost:8000/tweet', params={'id': id})
+        return r.text
+    
+main = Main()
+
+for i in range(0, 11):
+    print(i)
+    print(main.getTweet(i))
--- server/main.py
+++ server/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from utils.redis import Redis
+from algorithms.lfu import LFUAlgorithm
 
 app = FastAPI()
+redis = Redis()
+lfu = LFUAlgorithm(11)
 
 @app.get("/")
 def defaultEndpoint():
     return "Dude... lets move on to /tweet!!"
 
 @app.get("/tweet")
-def getTest():
-    return {"name": "Hello Baby"}
+def getTest(id: int):
+    tweet = lfu.get(id)
+    print(redis.getKeys())
+    return tweet
